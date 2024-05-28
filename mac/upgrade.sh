3d4fe8fd42dd7eb64d207772440116a3f7ce5ca0
Jacob Colvin <jacobcolvin1@gmail.com>
2024-05-28 02:29:33 -0400

    Remove devbox'ed packages

--- mac/upgrade.sh
+++ mac/upgrade.sh
@@ -5,8 +5,3 @@ brew cu -a
 fisher update
 k krew update
 k krew upgrade
-
-# Completions
-curl https://raw.githubusercontent.com/go-task/task/main/completion/fish/task.fish > .config/fish/completions/task.fish
-kyverno completion fish > .config/fish/completions/kyverno.fish
-cilium completion fish > .config/fish/completions/cilium.fish
