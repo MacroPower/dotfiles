f98cb94b2f2b59f035e9100bbc2f82dce4372489
Jacob Colvin <jacobcolvin1@gmail.com>
2025-12-26 12:42:46 -0500

    Remove pyenv, tfenv

--- mac/upgrade.sh
+++ mac/upgrade.sh
@@ -3,14 +3,6 @@
 brew bundle install --cleanup
 brew cu -y
 
-pyenv install --skip-existing 3.10
-pyenv install --skip-existing 3.11
-pyenv install --skip-existing 3.12
-pyenv install --skip-existing 3.13
-pyenv global 3.12
-
-tfenv install latest
-tfenv use latest
 
 fisher update
 krewfile --file=.krewfile --command="kubectl krew" --upgrade
