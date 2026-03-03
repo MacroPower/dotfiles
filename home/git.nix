1684010a6a292d560d5b88da00e3a1ea04a375d9
Jacob Colvin <jacobcolvin1@gmail.com>
2026-03-02 20:37:30 -0500

    Modernize git configuration

--- home/git.nix
+++ home/git.nix
@@ -11,6 +11,13 @@
       };
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
+      rerere.enabled = true;
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      push.autoSetupRemote = true;
+      rebase.autoStash = true;
+      fetch.prune = true;
+      init.defaultBranch = "main";
     };
 
     ignores = [
