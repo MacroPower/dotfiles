0a928cd3ee98456b5559be0e60379add2daf7695
Jacob Colvin <jacobcolvin1@gmail.com>
2026-03-20 16:27:56 -0400

    Add MacBook Pro host configuration

---
+++ hosts/darwin/mbp.nix
@@ -0,0 +1,46 @@
+{
+  username = "jacobcolvin";
+
+  loginItems = [
+    "/Applications/LinearMouse.app"
+    "~/Applications/Home Manager Apps/Caffeine.app"
+    "/Applications/OrbStack.app"
+  ];
+
+  extraApps = [
+    "discord"
+    "obsidian"
+  ];
+
+  homebrew = {
+    taps = [ ];
+    brews = [ ];
+    casks = [
+      "plex"
+      "orbstack"
+      "filebot"
+    ];
+    masApps = { };
+  };
+
+  homeModule =
+    { pkgs, ... }:
+    {
+      dotfiles = {
+        git = {
+          userName = "Jacob Colvin";
+          userEmail = "jacobcolvin1@gmail.com";
+        };
+        extraHomePackages = with pkgs; [
+          talosctl
+          discord
+          obsidian
+          slack
+        ];
+        vscode.extraExtensions =
+          marketplace: with marketplace; [
+            wakatime.vscode-wakatime
+          ];
+      };
+    };
+}
