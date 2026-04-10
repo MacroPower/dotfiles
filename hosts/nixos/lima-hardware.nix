b91b6a50ca6e1d0e1d1c50be2f3fbc719590b4d4
Jacob Colvin <jacobcolvin1@gmail.com>
2026-04-10 17:00:54 -0400

    feat: add iptables to Lima NixOS hardware packages

--- hosts/nixos/lima-hardware.nix
+++ hosts/nixos/lima-hardware.nix
@@ -14,6 +14,7 @@ nixos-lima:
   virtualisation.containerd.enable = true;
 
   environment.systemPackages = with pkgs; [
+    iptables
     nerdctl
     pkgsStatic.tini
     (writeShellScriptBin "docker" ''exec sudo ${nerdctl}/bin/nerdctl "$@"'')
